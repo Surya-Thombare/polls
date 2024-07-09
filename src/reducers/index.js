fe3d0d523c17ba46cda62d0d0e1587589f21cf71
Surya-Thombare <suryathombre000@gmail.com>
2024-07-10 01:02:45 +0530

    users

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import authedUser from './authUserReducer'
+import users from './users'
 
 export default combineReducers({
-  authedUser
+  authedUser,
+  users
 })
 
 
