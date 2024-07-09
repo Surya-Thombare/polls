c699346c43e048ac3424f3db0d239546c639189e
Surya-Thombare <suryathombre000@gmail.com>
2024-07-10 00:54:29 +0530

    Auth user

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import * as React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function App() {
+  const store = useSelector((store) => store)
+
+  console.log(store, 'Store');
   return (
     <div>
       Redux Polls
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+import authedUser from './authUserReducer'
+
+export default combineReducers({
+  authedUser
+})
+
+
+
