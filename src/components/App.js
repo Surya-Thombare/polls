2a61c3bc35f954d248a4eeb3eba3fd6fd86ef086
Surya-Thombare <suryathombre000@gmail.com>
2024-07-12 01:01:42 +0530

    Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Leaderboard from './leaderBoard'
 import Dashboard from './dashboard'
 import AddPoll from './addPoll'
+import Nav from './nav'
+import Poll from './Poll'
 // import AddPoll from './AddPoll'
 
 export default function App() {
@@ -18,9 +20,23 @@ export default function App() {
   return (
     <Router>
       <div className='container'>
+        <Nav />
         {loading === true
           ? null
-          : <AddPoll />
+          : <div>
+            <Route path='/' exact >
+              <Dashboard />
+            </Route>
+            <Route path='/leaderboard' exact >
+              <Leaderboard />
+            </Route>
+            <Route path='/add' exact >
+              <AddPoll />
+            </Route>
+            <Route path='/polls/:id' exact >
+              <Poll />
+            </Route>
+          </div>
         }
       </div>
     </Router>
